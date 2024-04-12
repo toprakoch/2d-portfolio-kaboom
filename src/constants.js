6de2197d2a00e28e9e0fb0c6619597efdb92b657
Nicat Safarov <nicati2004@outlook.com>
2024-04-12 16:09:19 +0400

    a

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC.`,
+  "cs-degree": `This is my infosec degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation. (from future)`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my family TV.`,
+  bed: `This where I sleep.`,
+  resume: `This is my desk and on it is my resume.(will add later :3 ) <a href="" target="_blank">Check it out?</a>
+  Contact me at seferovnicat3@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of books on my shelves. There is even one in French and I am learning French for fun :D.
+  I probably only read one of them.`,
+  exit: `If you want to exit toprakoch's portfolio, just close the tab.`,
+};
