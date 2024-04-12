30158a7f63054a741ec320971f7a6c240b9d483b
Nicat <41322242+toprakoch@users.noreply.github.com>
2024-04-12 16:18:52 +0400

    Update constants.js
    aztv

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const scaleFactor = 4;
 export const dialogueData = {
   pc: `This is my PC.`,
   "cs-degree": `This is my infosec degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation. (from future)`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch AZTV.`,
   tv: `That's my family TV.`,
   bed: `This where I sleep.`,
   resume: `This is my desk and on it is my resume.(will add later :3 ) <a href="" target="_blank">Check it out?</a>
