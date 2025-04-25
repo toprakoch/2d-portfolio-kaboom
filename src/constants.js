267d44c0023f422c89fa9f6e71fe159d2c8314e8
Nicat <41322242+toprakoch@users.noreply.github.com>
2025-04-25 17:09:52 +0400

    Update constants.js
    i was

--- src/constants.js
+++ src/constants.js
@@ -11,7 +11,7 @@ export const dialogueData = {
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
   Kaboom.js by watching tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of books on my shelves. There is even one in French and I am learning French for fun :D.
+  library: `There are a lot of books on my shelves. There is even one in French and I am learning French for fun (i was) :D.
   I probably only read one of them.`,
   exit: `If you want to exit toprakoch's portfolio, just close the tab.`,
 };
